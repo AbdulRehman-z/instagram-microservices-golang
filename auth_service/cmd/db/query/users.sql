25ebeb55e0264cbd72af9679981c61fafe20adc4
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-11-27 02:53:23 -0800

    Refactor user registration and update queries

--- auth_service/cmd/db/query/users.sql
+++ auth_service/cmd/db/query/users.sql
@@ -1,17 +1,15 @@
--- name: CreateUser :one
+-- name: RegisterUser :one
 INSERT INTO "users" (
-        "username",
-        "hashed_password",
-        "full_name",
-        "email"
+        "email",
+        "hashed_password"
     )
-VALUES ($1, $2, $3, $4)
+VALUES ($1, $2)
 RETURNING *;
 
 -- name: GetUser :one
 SELECT *
 FROM "users"
-WHERE "id" = $1
+WHERE "email" = $1 
 LIMIT 1;
 
 -- name: UpdateUser :one
@@ -19,7 +17,6 @@ UPDATE users
 SET 
     hashed_password = coalesce(sqlc.narg('hashed_password'), hashed_password),
     password_changed_at = coalesce(sqlc.narg('password_changed_at'), password_changed_at),
-    full_name = coalesce(sqlc.narg('full_name'), full_name),
     email = coalesce(sqlc.narg('email'), email),
     is_email_verified = coalesce(sqlc.narg('is_email_verified'), is_email_verified)
 WHERE username = sqlc.arg(username)
