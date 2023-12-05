5ce2f715d7691a6bdb78b51064ea6240b4ea5947
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-12-05 09:20:02 -0800

    Delete unnecessary code files

--- auth_service/cmd/token/maker.go
+++
@@ -1,8 +0,0 @@
-package token
-
-import "time"
-
-type TokenMaker interface {
-	CreateToken(email string, duration time.Duration) (string, *Payload, error)
-	VerifyToken(token string) (*Payload, error)
-}
