4bf35a30d759453b1bb2dff834896306180d095f
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-12-07 08:42:31 -0800

    Add taskDistributor to Server struct

--- auth_service/api/server.go
+++ auth_service/api/server.go
@@ -4,19 +4,21 @@ import (
 	db "github.com/AbdulRehman-z/instagram-microservices/auth_service/db/sqlc"
 	"github.com/AbdulRehman-z/instagram-microservices/auth_service/token"
 	"github.com/AbdulRehman-z/instagram-microservices/auth_service/util"
+	"github.com/AbdulRehman-z/instagram-microservices/auth_service/worker"
 	"github.com/gofiber/fiber/v2"
 	"github.com/redis/go-redis/v9"
 )
 
 type Server struct {
-	Config     util.Config
-	store      db.Store
-	rStore     *redis.Client
-	router     *fiber.App
-	tokenMaker token.TokenMaker
+	Config          util.Config
+	store           db.Store
+	rStore          *redis.Client
+	router          *fiber.App
+	tokenMaker      token.TokenMaker
+	taskDistributor worker.Distributor
 }
 
-func NewServer(config util.Config, db db.Store, redisClient *redis.Client) (*Server, error) {
+func NewServer(config util.Config, db db.Store, redisClient *redis.Client, taskDistributor worker.Distributor) (*Server, error) {
 	app := fiber.New()
 	tokenMaker, err := token.NewPaestoMaker(config.SYMMETRIC_KEY)
 	if err != nil {
@@ -24,11 +26,12 @@ func NewServer(config util.Config, db db.Store, redisClient *redis.Client) (*Ser
 	}
 
 	server := &Server{
-		Config:     config,
-		store:      db,
-		rStore:     redisClient,
-		router:     app,
-		tokenMaker: tokenMaker,
+		Config:          config,
+		store:           db,
+		rStore:          redisClient,
+		router:          app,
+		tokenMaker:      tokenMaker,
+		taskDistributor: taskDistributor,
 	}
 
 	server.SetupRoutes()
@@ -44,8 +47,9 @@ func (server *Server) Shutdown() error {
 }
 
 func (server *Server) SetupRoutes() {
-	// server.router.Get("/health", server.health)
-	// server.router.Post("/signup", server.signup)
+	server.router.Get("/health", server.Health)
+	server.router.Post("/signup", server.RegisterUser)
+	server.router.Post("/login", server.LoginUser)
 	// server.router.Post("/login", server.login)
 	// server.router.Post("/verify-email", server.verifyEmail)
 	// server.router.Post("/refresh", server.refresh)
