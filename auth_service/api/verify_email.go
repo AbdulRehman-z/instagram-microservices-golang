278bc48203d61989b1790495702da2199b327f68
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-12-08 09:51:37 -0800

    Add verifyemail route handler

---
+++ auth_service/api/verify_email.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	db "github.com/AbdulRehman-z/instagram-microservices/auth_service/db/sqlc"
+	"github.com/AbdulRehman-z/instagram-microservices/auth_service/types"
+	"github.com/AbdulRehman-z/instagram-microservices/auth_service/util"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (s *Server) VerifyEmail(c *fiber.Ctx) error {
+	var req types.VerifyEmailReqParams
+	err := c.QueryParser(&req)
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	if err := util.CheckValidationErrors(req); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	verifyTxResult, err := s.store.VerifyEmailTx(c.Context(), db.VerifyEmailTxParams{
+		EmailId:    int32(req.EmailId),
+		SecretCode: req.SecretCode,
+	})
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	resp := types.VerifyEmailResponse{
+		IsEmailVerified: verifyTxResult.User.IsEmailVerified && verifyTxResult.VerifyEmail.IsUsed,
+	}
+
+	return c.Status(fiber.StatusOK).JSON(&fiber.Map{
+		"data": resp,
+	})
+}
