785e06ed8c1fcf9688c7eb618bc59cb9ab5cf2ce
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-12-08 09:52:00 -0800

    add top level tx

--- auth_service/db/sqlc/store.go
+++ auth_service/db/sqlc/store.go
@@ -1,9 +1,14 @@
 package db
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
 
 type Store interface {
 	Querier
+	VerifyEmailTx(ctx context.Context, arg VerifyEmailTxParams) (*VerifyEmailTxResult, error)
 }
 
 type sqlStore struct {
@@ -16,4 +21,24 @@ func NewStore(db *sql.DB) Store {
 		Queries: New(db),
 		db:      db,
 	}
+}
+
+func (s *sqlStore) execTx(ctx context.Context, isolationLevel sql.IsolationLevel, fn func(*Queries) error) error {
+	tx, err := s.db.BeginTx(ctx, &sql.TxOptions{
+		Isolation: isolationLevel,
+	})
+	if err != nil {
+		return fmt.Errorf("tx: failed to begin transaction -- %w", err)
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if err := tx.Rollback(); err != nil {
+			return fmt.Errorf("tx: rolling back -- %w", err)
+		}
+		return err
+	}
+
+	return tx.Commit()
 }
