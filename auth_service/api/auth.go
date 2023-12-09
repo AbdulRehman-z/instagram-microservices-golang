949b6ae6d6d238add8fa71b61ae83209b8041615
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-12-09 09:44:33 -0800

    refactor register_user handler with tx

--- auth_service/api/auth.go
+++ auth_service/api/auth.go
@@ -8,7 +8,9 @@ import (
 	db "github.com/AbdulRehman-z/instagram-microservices/auth_service/db/sqlc"
 	"github.com/AbdulRehman-z/instagram-microservices/auth_service/types"
 	"github.com/AbdulRehman-z/instagram-microservices/auth_service/util"
+	"github.com/AbdulRehman-z/instagram-microservices/auth_service/worker"
 	"github.com/gofiber/fiber/v2"
+	"github.com/hibiken/asynq"
 	"github.com/lib/pq"
 )
 
@@ -27,12 +29,30 @@ func (server *Server) RegisterUser(c *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusInternalServerError, "internal err")
 	}
 
-	args := db.RegisterUserParams{
-		Email:          req.Email,
-		HashedPassword: hashedPassword,
+	args := db.RegisterUserTXParams{
+		RegisterUserParams: db.RegisterUserParams{
+			Email:          req.Email,
+			HashedPassword: hashedPassword,
+		},
+		AfterRegister: func(user db.User) error {
+			opts := []asynq.Option{
+				asynq.MaxRetry(10),
+				asynq.ProcessIn(10),
+				asynq.Queue(worker.TaskSignupEmailVerification),
+			}
+
+			payload := &worker.PayloadSendVerificationEmail{
+				Email: user.Email,
+			}
+			err := server.taskDistributor.TaskSendVerificationEmail(c.Context(), payload, opts...)
+			if err != nil {
+				return fmt.Errorf("cannot send verification email: %w", err)
+			}
+			return nil
+		},
 	}
 
-	user, err := server.store.RegisterUser(context.Background(), args)
+	userTx, err := server.store.RegisterUserTx(c.Context(), args)
 	if err != nil {
 		if pqerr, ok := err.(*pq.Error); ok {
 			switch pqerr.Code.Name() {
@@ -43,9 +63,10 @@ func (server *Server) RegisterUser(c *fiber.Ctx) error {
 	}
 
 	response := types.RegisterUserResParams{
-		Email:             user.Email,
-		PasswordChangedAt: user.PasswordChangedAt,
-		CreatedAt:         user.CreatedAt,
+
+		Email:             userTx.User.Email,
+		PasswordChangedAt: userTx.User.PasswordChangedAt,
+		CreatedAt:         userTx.User.CreatedAt,
 	}
 	return c.Status(fiber.StatusCreated).JSON(&fiber.Map{
 		"data": response,
@@ -91,6 +112,10 @@ func (server *Server) LoginUser(c *fiber.Ctx) error {
 	fmt.Println(res)
 	return nil
 }
+
+// func (s *Server) UpdateUser(ctx *fiber.Ctx) error {
+// 	var
+// }
 
 func (s *Server) Health(ctx *fiber.Ctx) error {
 	return ctx.SendString("OK")
