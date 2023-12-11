6b7f3e67a6c7d9ebc5d28556d351710a125f8792
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-12-11 08:03:14 -0800

    Add time duration for task processing in RegisterUser function

--- auth_service/api/auth.go
+++ auth_service/api/auth.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"time"
 
 	db "github.com/AbdulRehman-z/instagram-microservices/auth_service/db/sqlc"
 	"github.com/AbdulRehman-z/instagram-microservices/auth_service/types"
@@ -37,14 +38,14 @@ func (server *Server) RegisterUser(c *fiber.Ctx) error {
 		AfterRegister: func(user db.User) error {
 			opts := []asynq.Option{
 				asynq.MaxRetry(10),
-				asynq.ProcessIn(10),
+				asynq.ProcessIn(10 * time.Second),
 				asynq.Queue(worker.TaskSignupVerificationEmail),
 			}
 
 			payload := &worker.PayloadSendVerificationEmail{
 				Email: user.Email,
 			}
-			err := server.taskDistributor.TaskSendSignupEmail(c.Context(), payload, opts...)
+			err := server.taskDistributor.TaskSendSignupEmail(c.UserContext(), payload, opts...)
 			if err != nil {
 				return fmt.Errorf("cannot send verification email: %w", err)
 			}
@@ -52,21 +53,20 @@ func (server *Server) RegisterUser(c *fiber.Ctx) error {
 		},
 	}
 
-	userTx, err := server.store.RegisterUserTx(c.Context(), args)
+	userTx, err := server.store.RegisterUserTx(c.UserContext(), args)
 	if err != nil {
-		if pqerr, ok := err.(*pq.Error); ok {
-			switch pqerr.Code.Name() {
-			case "unique_violation", "foreign_key_violation":
-				return fiber.NewError(fiber.StatusBadRequest, "user already registered")
+		if pqError, ok := err.(*pq.Error); ok {
+			if pqError.Code.Name() == "unique_violation" {
+				return fiber.NewError(fiber.StatusConflict, err.Error())
 			}
 		}
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
 
 	response := types.RegisterUserResParams{
-
 		Email:             userTx.User.Email,
 		PasswordChangedAt: userTx.User.PasswordChangedAt,
-		CreatedAt:         userTx.User.CreatedAt,
+		PasswordCreatedAt: userTx.User.CreatedAt,
 	}
 	return c.Status(fiber.StatusCreated).JSON(&fiber.Map{
 		"data": response,
