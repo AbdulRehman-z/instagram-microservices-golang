1c6e82a50c806863489731c8002f3d8f9c57950f
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-12-15 07:59:43 -0800

    Add renewAccessTokenHandler function to token.go

---
+++ auth_service/api/token.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"github.com/AbdulRehman-z/instagram-microservices/auth_service/types"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (server *Server) renewAccessTokenHandler(c *fiber.Ctx) error {
+	var req types.RenewAccessTokenRequest
+	if err := c.BodyParser(&req); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "invalid request body")
+	}
+
+	refreshTokenPayload, err := server.tokenMaker.VerifyToken(req.RefreshToken)
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnauthorized, "unauthorized")
+	}
+	// get session from redis
+	session, err := server.rStore.HGetAll(c.UserContext(), refreshTokenPayload.Email).Result()
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	if session["IsBlocked"] == "true" {
+		return fiber.NewError(fiber.StatusUnauthorized, "unauthorized")
+	}
+
+	if session["Email"] != refreshTokenPayload.Email {
+		return fiber.NewError(fiber.StatusUnauthorized, "unauthorized")
+	}
+
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(refreshTokenPayload.Email, server.Config.ACCESS_TOKEN_DURATION)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	response := types.RenewAccessTokenResponse{
+		AccessToken:          accessToken,
+		AccessTokenExpiresAt: accessPayload.ExpiredAt,
+	}
+
+	return c.Status(fiber.StatusOK).JSON(&fiber.Map{
+		"data": response,
+	})
+}
