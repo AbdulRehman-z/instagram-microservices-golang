5fd1a060d5127f8b9d1100c2c4db5188c7b8d9eb
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-12-23 09:22:17 -0800

    Add initial SQL files for posts service

---
+++ posts_service/db/query/posts.sql
@@ -0,0 +1,22 @@
+-- name: CreatePost :one
+INSERT INTO "posts" (
+    "unique_id"
+
+)
+
+
+
+-- -- name: RegisterUser :one
+-- INSERT INTO "users" (
+--         "email",
+--         "hashed_password"
+--     )
+-- VALUES ($1, $2)
+-- RETURNING *;
+
+
+
+-- TODO: 
+-- 1. Write queries
+-- 2. Generate functions from queries
+-- 3. Write api endpoints
