cfb5860cc7a4f929da9e7abb37794c41686fb048
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-12-23 09:19:22 -0800

    Add account table and queries

---
+++ user-profile_service/db/migration/000001_create_account.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS "accounts";
+DROP INDEX IF EXISTS "accounts_id_idx";
+DROP INDEX IF EXISTS "accounts_unique_id_idx";
+DROP INDEX IF EXISTS "accounts_email_idx";
---
+++ user-profile_service/db/migration/000001_create_account.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE accounts (
+    id serial PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    unique_id VARCHAR(255) UNIQUE NOT NULL,
+    age INT NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    bio TEXT NOT NULL,
+    avatar VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX account_id_idx ON accounts(id);
+CREATE INDEX account_unique_id_idx ON accounts(unique_id);
+CREATE INDEX account_email_idx ON accounts(email);
