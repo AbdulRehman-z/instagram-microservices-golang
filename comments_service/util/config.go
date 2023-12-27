6fc4080414717c67e24d1851cc8c0b1f7e66822b
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2023-12-27 09:30:04 -0800

    add util pkg

---
+++ comments_service/util/config.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DB_DRIVER              string        `mapstructure:"DB_DRIVER"`
+	DB_URL                 string        `mapstructure:"DB_URL"`
+	SYMMETRIC_KEY          string        `mapstructure:"SYMMETRIC_KEY"`
+	LISTEN_ADDR            string        `mapstructure:"LISTEN_ADDR"`
+	ACCESS_TOKEN_DURATION  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	DB_MIGRATION_URL       string        `mapstructure:"DB_MIGRATION_URL"`
+	REFRESH_TOKEN_DURATION time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+	REDIS_ADDR             string        `mapstructure:"REDIS_ADDR"`
+	APP_PASSWORD           string        `mapstructure:"APP_PASSWORD"`
+	REDIS_HOST             string        `mapstructure:"REDIS_HOST"`
+	REDIS_PORT             string        `mapstructure:"REDIS_PORT"`
+	REDIS_PASSWORD         string        `mapstructure:"REDIS_PASSWORD"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	var config Config
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
