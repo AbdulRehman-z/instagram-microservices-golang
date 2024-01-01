3ed4db3dd34bbee4fe85ebd6185270fb34341eea
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-01-01 09:17:58 -0800

    Add publisher function to user-profile_service/api

---
+++ user-profile_service/api/publisher.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func (s *Server) Publisher(uniqueId string) error {
+	fmt.Println("||||||||||||| PUBLISHER ||||||||||||||")
+	var (
+		USER_PROFILE_STREAM = "user_profile_stream"
+	)
+
+	res, err := s.redisClient.XAdd(context.Background(), &redis.XAddArgs{
+		Values: map[string]any{
+			"unique_id": uniqueId,
+		},
+		ID:     "*",
+		Stream: USER_PROFILE_STREAM,
+	}).Result()
+	fmt.Println(res)
+	if err != nil {
+		return fmt.Errorf("error adding to stream: %d", err)
+	}
+	log.Printf("added to stream: %s", res)
+	return nil
+}
