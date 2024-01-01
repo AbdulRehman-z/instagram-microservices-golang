259a0265ce45bbcf4f761ca8a2f64c09b9db0980
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-01-01 09:18:16 -0800

    Refactor user profile API

--- user-profile_service/api/user-profile.go
+++ user-profile_service/api/user-profile.go
@@ -1,14 +1,10 @@
 package api
 
 import (
-	"context"
-	"fmt"
-	"log"
 	"sync"
 
 	"github.com/AbdulRehman-z/instagram-microservices/user-profile_service/token"
 	"github.com/gofiber/fiber/v2"
-	"github.com/redis/go-redis/v9"
 )
 
 type Profile struct {
@@ -35,123 +31,123 @@ func (s *Server) UserProfile(ctx *fiber.Ctx) error {
 
 	totalFollowersChan := make(chan int32)
 	totalFollowingChan := make(chan int32)
-	totalPostsChan := make(chan int32)
-	postsChan := make(chan []Post)
-	accountChan := make(chan Account)
-
+	// totalPostsChan := make(chan int32)
+	// postsChan := make(chan []Post)
+	// accountChan := make(chan Account)
+	s.Publisher(uniqueId.String())
 	wg := sync.WaitGroup{}
-	go GetFollowersAndFollowinCount(s, uniqueId.String(), &wg, totalFollowersChan, totalFollowingChan)
-	go GetPostsAndPostsCount(s, uniqueId.String(), &wg, totalPostsChan, postsChan)
-	go GetAccount(s, uniqueId.String(), &wg, accountChan)
+	// go GetFollowersAndFollowinCount(s, uniqueId.String(), &wg, totalFollowersChan, totalFollowingChan)
+	// go GetPostsAndPostsCount(s, uniqueId.String(), &wg, totalPostsChan, postsChan)
+	// go GetAccount(s, uniqueId.String(), &wg, accountChan)
 	wg.Add(5)
 	wg.Wait()
 
 	profile := Profile{
-		UserInfo:       <-accountChan,
+		// UserInfo:       <-accountChan,
 		TotlaFollowers: <-totalFollowersChan,
 		TotalFollowing: <-totalFollowingChan,
-		TotalPosts:     <-totalPostsChan,
-		Posts:          <-postsChan,
+		// TotalPosts:     <-totalPostsChan,
+		// Posts:          <-postsChan,
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(profile)
 }
 
-func GetFollowersAndFollowinCount(s *Server, uniqueId string, wg *sync.WaitGroup, totalFollowersChan chan int32, totalFollowingChan chan int32) {
-	defer wg.Done()
-	var (
-		FOLLOWERS_FOLLOWING_COUNT = "followers_following_count"
-	)
+// func GetFollowersAndFollowinCount(s *Server, uniqueId string, wg *sync.WaitGroup, totalFollowersChan chan int32, totalFollowingChan chan int32) {
+// 	defer wg.Done()
+// 	var (
+// 		FOLLOWERS_FOLLOWING_COUNT = "followers_following_count"
+// 	)
 
-	if err := s.publish(uniqueId); err != nil {
-		log.Printf("error publishing to stream: %d", err)
-	}
+// 	if err := s.publish(uniqueId); err != nil {
+// 		log.Printf("error publishing to stream: %d", err)
+// 	}
 
-	// wait for the response from the consumer
-	for {
-		response, err := s.redisClient.XRead(context.Background(), &redis.XReadArgs{
-			Streams: []string{FOLLOWERS_FOLLOWING_COUNT, "0"}, //
-			Block:   0,
-		}).Result()
-		if err != nil {
-			log.Printf("error reading from stream: %d", err)
-			continue
-		}
+// // wait for the response from the consumer
+// for {
+// 	response, err := s.redisClient.XRead(context.Background(), &redis.XReadArgs{
+// 		Streams: []string{FOLLOWERS_FOLLOWING_COUNT, "0"}, //
+// 		Block:   0,
+// 	}).Result()
+// 	if err != nil {
+// 		log.Printf("error reading from stream: %d", err)
+// 		continue
+// 	}
 
-		for _, message := range response[0].Messages {
-			if message.Values["unique_id"] == uniqueId {
-				totalFollowersChan <- message.Values["total_followers"].(int32)
-				totalFollowingChan <- message.Values["total_following"].(int32)
-				fmt.Printf("total followers: %d", message.Values["total_followers"].(int32))
-				return
-			}
-		}
-		break
-	}
-}
+// 	for _, message := range response[0].Messages {
+// 		if message.Values["unique_id"] == uniqueId {
+// 			totalFollowersChan <- message.Values["total_followers"].(int32)
+// 			totalFollowingChan <- message.Values["total_following"].(int32)
+// 			fmt.Printf("total followers: %d", message.Values["total_followers"].(int32))
+// 			return
+// 		}
+// 	}
+// 	break
+// }
+// }
 
-func GetPostsAndPostsCount(s *Server, uniqueId string, wg *sync.WaitGroup, totalPostsChan chan int32, posts chan []Post) {
-	defer wg.Done()
-	var (
-		POSTS = "posts"
-	)
+// func GetPostsAndPostsCount(s *Server, uniqueId string, wg *sync.WaitGroup, totalPostsChan chan int32, posts chan []Post) {
+// 	defer wg.Done()
+// 	var (
+// 		POSTS = "posts"
+// 	)
 
-	if err := s.publish(uniqueId); err != nil {
-		log.Printf("error publishing to stream: %d", err)
-	}
+// 	if err := s.publish(uniqueId); err != nil {
+// 		log.Printf("error publishing to stream: %d", err)
+// 	}
 
-	// wait for the response from the consumer
-	for {
-		response, err := s.redisClient.XRead(context.Background(), &redis.XReadArgs{
-			Streams: []string{POSTS, "0"},
-			Block:   0,
-		}).Result()
-		if err != nil {
-			log.Printf("error reading from stream: %d", err)
-			continue
-		}
+// 	// wait for the response from the consumer
+// 	for {
+// 		response, err := s.redisClient.XRead(context.Background(), &redis.XReadArgs{
+// 			Streams: []string{POSTS, "0"},
+// 			Block:   0,
+// 		}).Result()
+// 		if err != nil {
+// 			log.Printf("error reading from stream: %d", err)
+// 			continue
+// 		}
 
-		for _, message := range response[0].Messages {
-			if message.Values["unique_id"] == uniqueId {
-				totalPostsChan <- message.Values["total_posts"].(int32)
-				posts <- message.Values["posts"].([]Post)
-				fmt.Printf("total posts: %d", message.Values["total_posts"].(int32))
-				return
-			}
-		}
-	}
-}
+// 		for _, message := range response[0].Messages {
+// 			if message.Values["unique_id"] == uniqueId {
+// 				totalPostsChan <- message.Values["total_posts"].(int32)
+// 				posts <- message.Values["posts"].([]Post)
+// 				fmt.Printf("total posts: %d", message.Values["total_posts"].(int32))
+// 				return
+// 			}
+// 		}
+// 	}
+// }
 
-func GetAccount(s *Server, uniqueId string, wg *sync.WaitGroup, accountChan chan Account) {
-	defer wg.Done()
-	var (
-		ACCOUNT = "account"
-	)
+// func GetAccount(s *Server, uniqueId string, wg *sync.WaitGroup, accountChan chan Account) {
+// 	defer wg.Done()
+// 	var (
+// 		ACCOUNT = "account"
+// 	)
 
-	if err := s.publish(uniqueId); err != nil {
-		log.Printf("error publishing to stream: %d", err)
-	}
+// 	if err := s.publish(uniqueId); err != nil {
+// 		log.Printf("error publishing to stream: %d", err)
+// 	}
 
-	// wait for the response from the consumer
-	for {
-		response, err := s.redisClient.XRead(context.Background(), &redis.XReadArgs{
-			Streams: []string{ACCOUNT, "0"}, //
-			Block:   0,
-		}).Result()
-		if err != nil {
-			log.Printf("error reading from stream: %d", err)
-			continue
-		}
+// 	// wait for the response from the consumer
+// 	for {
+// 		response, err := s.redisClient.XRead(context.Background(), &redis.XReadArgs{
+// 			Streams: []string{ACCOUNT, "0"}, //
+// 			Block:   0,
+// 		}).Result()
+// 		if err != nil {
+// 			log.Printf("error reading from stream: %d", err)
+// 			continue
+// 		}
 
-		for _, message := range response[0].Messages {
-			if message.Values["unique_id"] == uniqueId {
-				accountChan <- Account{
-					Username: message.Values["username"].(string),
-					ImageUrl: message.Values["image_url"].(string),
-				}
-				fmt.Printf("username: %s", message.Values["username"].(string))
-				return
-			}
-		}
-	}
-}
+// 		for _, message := range response[0].Messages {
+// 			if message.Values["unique_id"] == uniqueId {
+// 				accountChan <- Account{
+// 					Username: message.Values["username"].(string),
+// 					ImageUrl: message.Values["image_url"].(string),
+// 				}
+// 				fmt.Printf("username: %s", message.Values["username"].(string))
+// 				return
+// 			}
+// 		}
+// 	}
+// }
