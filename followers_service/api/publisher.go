da734e9baa44b05775bf77456e1ad0af05ddf668
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-01-02 10:16:42 -0800

    Refactor publisher function to publish followers and following count

--- followers_service/api/publisher.go
+++ followers_service/api/publisher.go
@@ -10,60 +10,31 @@ import (
 
 func (s *Server) Publisher() error {
 	fmt.Println("||||||||||||| PUBLISHER ||||||||||||||")
-	for {
-		select {
-		case followersCount := <-s.followersCountChan:
-			fmt.Println("----------------------------")
-			fmt.Println("followers count: ", followersCount)
-			fmt.Println("----------------------------")
-			if err := s.publishFollowersCount(s.uniqueId, followersCount); err != nil {
-				log.Printf("error publishing to stream: %d", err)
-			}
-		case followingCount := <-s.followingCountChan:
-			fmt.Println("----------------------------")
-			fmt.Println("following count: ", followingCount)
-			fmt.Println("----------------------------")
-			if err := s.publishFollowingCount(s.uniqueId, followingCount); err != nil {
-				log.Printf("error publishing to stream: %d", err)
-			}
+	for followersFollowingCount := range s.followersFollowingCountChan {
+		fmt.Println("----------------------------")
+		fmt.Println("followers and following count published: ", followersFollowingCount)
+		fmt.Println("----------------------------")
+		if err := s.publishFollowersCount(s.uniqueId, followersFollowingCount); err != nil {
+			log.Printf("error publishing to stream: %d", err)
 		}
 	}
-}
-
-func (s *Server) publishFollowersCount(uniqueId string, followersCount int64) error {
-	var (
-		FOLLOWERS_FOLLOWING_COUNT = "followers_following_count"
-	)
-	res, err := s.redisClient.XAdd(context.Background(), &redis.XAddArgs{
-		Values: map[string]interface{}{
-			"unique_id":       uniqueId,
-			"total_followers": followersCount,
-		},
-		ID:     "*",
-		Stream: FOLLOWERS_FOLLOWING_COUNT,
-	}).Result()
-	if err != nil {
-		return fmt.Errorf("error adding to stream: %d", err)
-	}
-	log.Printf("added to stream: %s", res)
 	return nil
 }
 
-func (s *Server) publishFollowingCount(uniqueId string, followingCount int64) error {
+func (s *Server) publishFollowersCount(uniqueId string, followersFollowingCount string) error {
 	var (
-		FOLLOWERS_FOLLOWING_COUNT = "followers_following_count"
+		FOLLOWERS_FOLLOWING_COUNT_STREAM = "followers_following_count_stream"
 	)
-	res, err := s.redisClient.XAdd(context.Background(), &redis.XAddArgs{
+	_, err := s.redisClient.XAdd(context.Background(), &redis.XAddArgs{
 		Values: map[string]interface{}{
-			"unique_id":       uniqueId,
-			"total_following": followingCount,
+			"unique_id":                 uniqueId,
+			"total_followers_following": followersFollowingCount,
 		},
 		ID:     "*",
-		Stream: FOLLOWERS_FOLLOWING_COUNT,
+		Stream: FOLLOWERS_FOLLOWING_COUNT_STREAM,
 	}).Result()
 	if err != nil {
 		return fmt.Errorf("error adding to stream: %d", err)
 	}
-	log.Printf("added to stream: %s", res)
 	return nil
 }
