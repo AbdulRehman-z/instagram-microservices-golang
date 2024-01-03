c2289a30b6f3f270245d1baa3db9535f257df364
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-01-03 10:05:51 -0800

    Add listener function to posts_service/api/listener.go

---
+++ posts_service/api/listener.go
@@ -0,0 +1,59 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func (s *Server) Listener() {
+	var (
+		USER_PROFILE_STREAM = "user_profile_stream"
+	)
+	for {
+		streams, err := s.redisClient.XRead(context.Background(), &redis.XReadArgs{
+			Streams: []string{USER_PROFILE_STREAM, "$"},
+			Block:   0,
+		}).Result()
+		if err != nil {
+			log.Printf("err reading from stream: %s || error: %s\n", USER_PROFILE_STREAM, err)
+		}
+		fmt.Println("|||||||||||------ POSTS LISTENER STARTED! |||||||||||------")
+
+		for _, stream := range streams {
+			fmt.Println("stream: ", stream)
+			for _, message := range stream.Messages {
+				fmt.Println("message: ", message)
+				s.uniqueId = message.Values["unique_id"].(string)
+				getPostsAndTotalPostsCount(s)
+				log.Printf("listened event from stream: %s || event: %s", USER_PROFILE_STREAM, message)
+			}
+		}
+	}
+}
+
+func getPostsAndTotalPostsCount(s *Server) {
+	posts := []Post{
+		{
+			PostId: 1,
+			Url:    "image1.jpg",
+		},
+		{
+			PostId: 2,
+			Url:    "image2.jpg",
+		},
+		{
+			PostId: 3,
+			Url:    "image3.jpg",
+		},
+	}
+
+	event := &PostEvent{
+		UniqueId:   s.uniqueId,
+		TotalPosts: 782,
+		Posts:      posts,
+	}
+	s.PostsChan <- event
+}
