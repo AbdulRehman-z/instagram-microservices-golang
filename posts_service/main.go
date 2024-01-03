74f16aa2d411d9b2aa8ba3de7c3dacc5d5a95341
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-01-03 10:05:44 -0800

    Add Redis client and update server initialization

--- posts_service/main.go
+++ posts_service/main.go
@@ -8,6 +8,7 @@ import (
 	db "github.com/AbdulRehman-z/instagram-microservices/posts_service/db/sqlc"
 	"github.com/AbdulRehman-z/instagram-microservices/posts_service/util"
 	_ "github.com/lib/pq"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
@@ -20,20 +21,29 @@ func main() {
 	if err != nil {
 		log.Printf("Cannot connect to DB: %v", err)
 	}
-	util.RunMigration(config.DB_MIGRATION_URL, config.DB_URL)
 	if err := conn.Ping(); err != nil {
 		log.Printf("Cannot ping DB: %v", err)
 	}
 
+	// util.RunMigration(config.DB_MIGRATION_URL, config.DB_URL)
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     config.REDIS_ADDR,
+		Password: config.REDIS_PASSWORD,
+		DB:       0,
+	})
+
 	store := db.NewStore(conn)
-	run(config, store)
+	run(config, store, redisClient)
 }
 
-func run(config *util.Config, store db.Store) {
-	server, err := api.NewServer(*config, store)
+func run(config *util.Config, store db.Store, redisClient *redis.Client) {
+	server, err := api.NewServer(*config, store, redisClient)
 	if err != nil {
 		log.Fatalf("failed to initiate server: %d", err)
 	}
+	go server.Listener()
+	go server.Publisher()
 	err = server.Start(config.LISTEN_ADDR)
 	if err != nil {
 		log.Fatalf("failed to start server: %d", err)
