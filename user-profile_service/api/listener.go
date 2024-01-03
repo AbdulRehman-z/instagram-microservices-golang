3c65623164fb07030ea0b12eb56518347656edfb
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-01-03 10:06:12 -0800

    Refactor listeners in user-profile_service/api/listener.go

--- user-profile_service/api/listener.go
+++ user-profile_service/api/listener.go
@@ -8,37 +8,47 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-func (s *Server) Listener() error {
-	fmt.Println("|||||||||||||------- USER-PROFILE-SERVICE LISTENER STARTED! -------||||||||||||||")
+func (s *Server) FollowersListener() error {
+	fmt.Println("|||||||||||||------- FOLLOWERS LISTENER STARTED! -------||||||||||||||")
 	var (
 		FOLLOWERS_FOLLOWING_COUNT_STREAM = "followers_following_count_stream"
-		FOLLOWERS_FOLLOWING_GROUP        = "followers_following_group"
-		FOLLOWERS_FOLLOWING_CONSUMER_1   = "followers_following_consumer_1"
 	)
 
-	_, err := s.redisClient.XGroupCreate(context.Background(), FOLLOWERS_FOLLOWING_COUNT_STREAM,
-		FOLLOWERS_FOLLOWING_GROUP, "0").Result()
-	if err.Error() == "BUSYGROUP Consumer Group name already exists" {
-		log.Printf("group already exists: %s", FOLLOWERS_FOLLOWING_GROUP)
-	} else {
-		return fmt.Errorf("err creating group: %d", err)
-	}
-
 	for {
-		streams, err := s.redisClient.XReadGroup(context.Background(), &redis.XReadGroupArgs{
-			Streams:  []string{FOLLOWERS_FOLLOWING_COUNT_STREAM, ">"},
-			Group:    FOLLOWERS_FOLLOWING_GROUP,
-			Consumer: FOLLOWERS_FOLLOWING_CONSUMER_1,
-			Block:    0,
-			NoAck:    false,
+		streams, err := s.redisClient.XRead(context.Background(), &redis.XReadArgs{
+			Streams: []string{FOLLOWERS_FOLLOWING_COUNT_STREAM, "$"},
+			Block:   0,
 		}).Result()
 		if err != nil {
-			fmt.Printf("err creating group: %d", err)
+			fmt.Printf("err creating : %d", err)
 		}
 
 		for _, stream := range streams {
 			for _, message := range stream.Messages {
 				fmt.Println("message: ", message)
+			}
+		}
+	}
+}
+
+func (s *Server) PostsListener() error {
+	fmt.Println("|||||||||||||------- POSTS LISTENER STARTED! -------||||||||||||||")
+	var (
+		POSTS_STREAM = "posts_stream"
+	)
+
+	for {
+		streams, err := s.redisClient.XRead(context.Background(), &redis.XReadArgs{
+			Streams: []string{POSTS_STREAM, "$"},
+			Block:   0,
+		}).Result()
+		if err != nil {
+			log.Printf("err reading from stream:%s || err: %s\n", POSTS_STREAM, err)
+		}
+
+		for _, stream := range streams {
+			for _, message := range stream.Messages {
+				log.Println("message: ", message)
 			}
 		}
 	}
