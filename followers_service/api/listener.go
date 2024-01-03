92b8dfba566efad3ae9f02ce37789275ef062041
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-01-03 10:05:21 -0800

    Refactor listener function and remove unused code

--- followers_service/api/listener.go
+++ followers_service/api/listener.go
@@ -3,38 +3,33 @@ package api
 import (
 	"context"
 	"fmt"
-	"sync"
+	"log"
 
-	"github.com/gofiber/fiber/v2/log"
 	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 )
 
 func (s *Server) Listener() error {
 	fmt.Println("||||||||||||| LISTENER ||||||||||||||")
-	wg := sync.WaitGroup{}
 	var (
-		USER_PROFILE_STREAM     = "user_profile_stream"
-		USER_PROFILE_GROUP      = "user_profile_group"
-		USER_PROFILE_CONSUMER_1 = "user_profile_consumer_1"
+		USER_PROFILE_STREAM = "user_profile_stream"
+		// USER_PROFILE_GROUP      = "user_profile_group"
+		// USER_PROFILE_CONSUMER_1 = "user_profile_consumer_1"
 	)
 
-	_, err := s.redisClient.XGroupCreate(context.Background(), USER_PROFILE_STREAM, USER_PROFILE_GROUP, "0").Result()
-	if err != nil {
-		if err.Error() == "BUSYGROUP Consumer Group name already exists" {
-			fmt.Printf("group already exists: %s\n", USER_PROFILE_GROUP)
-		} else {
-			return fmt.Errorf("err creating group: %d", err)
-		}
-	}
+	// _, err := s.redisClient.XGroupCreate(context.Background(), USER_PROFILE_STREAM, USER_PROFILE_GROUP, "0").Result()
+	// if err != nil {
+	// 	if err.Error() == "BUSYGROUP Consumer Group name already exists" {
+	// 		fmt.Printf("group already exists: %s\n", USER_PROFILE_GROUP)
+	// 	} else {
+	// 		return fmt.Errorf("err creating group: %d", err)
+	// 	}
+	// }
 
 	for {
-		streams, err := s.redisClient.XReadGroup(context.Background(), &redis.XReadGroupArgs{
-			Streams:  []string{USER_PROFILE_STREAM, ">"},
-			Group:    USER_PROFILE_GROUP,
-			Consumer: USER_PROFILE_CONSUMER_1,
-			Block:    0,
-			NoAck:    false,
+		streams, err := s.redisClient.XRead(context.Background(), &redis.XReadArgs{
+			Streams: []string{USER_PROFILE_STREAM, "$"},
+			Block:   0,
 		}).Result()
 		if err != nil {
 			return fmt.Errorf("error reading from stream: %d", err)
@@ -44,27 +39,22 @@ func (s *Server) Listener() error {
 			for _, stream := range streams {
 				for _, message := range stream.Messages {
 					s.uniqueId = message.Values["unique_id"].(string)
-					s.redisClient.XAck(context.Background(), USER_PROFILE_STREAM, stream.Messages[0].ID)
-					go getFollowersCountFromStore(s, &wg, s.uniqueId)
-					// go getFollowingCountFromStore(s, &wg, s.followingCountChan, s.uniqueId)
-					wg.Add(1)
+					getFollowersAndFollowingCountFromStore(s, s.uniqueId)
+					log.Printf("listened event from stream: %s || event: %s", USER_PROFILE_STREAM, message)
 				}
 			}
 		}
 	}
 }
 
-func getFollowersCountFromStore(s *Server, wg *sync.WaitGroup, uniqueId string) {
-	defer func() {
-		wg.Done()
-	}()
+func getFollowersAndFollowingCountFromStore(s *Server, uniqueId string) {
 	uniqueIdUUID, err := uuid.Parse(s.uniqueId)
 	if err != nil {
-		log.Errorf("err parsing uuid: %d", uniqueIdUUID)
+		log.Println("err parsing uuid: ", err)
 	}
 	count, err := s.store.GetFollowingAndFollowersCount(context.Background(), uniqueIdUUID)
 	if err != nil {
-		log.Errorf("err getting following count from db: %d", err)
+		log.Println("err getting followers and following count: ", err)
 	}
 	s.followersFollowingCountChan <- fmt.Sprintf("%d", count)
 }
