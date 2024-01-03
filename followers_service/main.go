e35f33483951956959a7cd0c19ee19604c2cc70d
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-01-03 10:05:15 -0800

    Add Redis client to followers service

--- followers_service/main.go
+++ followers_service/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/AbdulRehman-z/instagram-microservices/followers_service/api"
 	db "github.com/AbdulRehman-z/instagram-microservices/followers_service/db/sqlc"
 	"github.com/AbdulRehman-z/instagram-microservices/followers_service/util"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
@@ -21,12 +22,18 @@ func main() {
 		log.Fatalf("cannot connect to db: %v", err)
 	}
 
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     config.REDIS_ADDR,
+		Password: config.REDIS_PASSWORD,
+		DB:       0,
+	})
+
 	store := db.NewStore(conn)
-	run(config, store)
+	run(config, store, redisClient)
 }
 
-func run(config *util.Config, store db.Store) error {
-	server, err := api.NewServer(config, store)
+func run(config *util.Config, store db.Store, redisClient *redis.Client) error {
+	server, err := api.NewServer(config, store, redisClient)
 	if err != nil {
 		log.Fatalf("cannot create server: %v", err)
 	}
