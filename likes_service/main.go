2c7950ff45fb266df225294a239030891f20d8fb
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-01-04 08:57:27 -0800

    Add Redis client and start server

--- likes_service/main.go
+++ likes_service/main.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/AbdulRehman-z/instagram-microservices/likes_service/api"
+	db "github.com/AbdulRehman-z/instagram-microservices/likes_service/db/sqlc"
+	"github.com/AbdulRehman-z/instagram-microservices/likes_service/util"
+	"github.com/redis/go-redis/v9"
+)
+
+func testDb() *sql.DB {
+	return &sql.DB{}
+}
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatalf("err loading config: %d", err)
+	}
+
+	conn := testDb()
+	store := db.NewStore(conn)
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     config.REDIS_ADDR,
+		Password: config.REDIS_PASSWORD,
+		DB:       0,
+	})
+
+	run(config, store, redisClient)
+}
+
+func run(config *util.Config, store db.Store, redisClient *redis.Client) {
+	server, err := api.NewServer(*config, store, redisClient)
+	if err != nil {
+		log.Fatal(err)
+	}
+	go server.CommentsLikesListener()
+	go server.PostsLikesListener()
+	go server.CommentsLikesPublisher()
+	go server.PostsLikesPublisher()
+	err = server.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
