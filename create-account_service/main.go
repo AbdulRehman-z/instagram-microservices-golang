05f1ba4084f7708cab5103d532a952c2486038c3
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-01-04 08:57:05 -0800

    Add Redis client and update server initialization

--- create-account_service/main.go
+++ create-account_service/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/AbdulRehman-z/instagram-microservices/create-account_service/api"
 	db "github.com/AbdulRehman-z/instagram-microservices/create-account_service/db/sqlc"
 	"github.com/AbdulRehman-z/instagram-microservices/create-account_service/util"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
@@ -26,18 +27,25 @@ func main() {
 	}
 
 	defer conn.Close()
-	util.RunMigration(config.DB_MIGRATION_URL, config.DB_URL)
+	// util.RunMigration(config.DB_MIGRATION_URL, config.DB_URL)
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     config.REDIS_ADDR,
+		Password: config.REDIS_PASSWORD,
+		DB:       0,
+	})
 
 	store := db.NewStore(conn)
-	run(config, store)
+	run(config, store, redisClient)
 }
 
-func run(config *util.Config, store db.Store) {
-	server, err := api.NewServer(*config, store)
+func run(config *util.Config, store db.Store, redisClient *redis.Client) {
+	server, err := api.NewServer(*config, store, redisClient)
 	if err != nil {
 		slog.Error("Cannot create server: ", err)
 	}
-
+	go server.Publisher()
+	go server.UserProfileListener()
 	err = server.Start(config.LISTEN_ADDR)
 	if err != nil {
 		slog.Error("Failed to start server: ", err)
